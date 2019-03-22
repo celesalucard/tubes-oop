3f28bf13a5c85f6de70b986075c257fba83fda78
celesalucard <celestia.alucard@gmail.com>
2019-03-22 21:44:07 +0700

    added comments to Utilities.java

--- Utilities.java
+++ Utilities.java
@@ -1,8 +1,14 @@
 public class Utilities extends Properties{
+    //Utilities merupakan turunan dari kelas Property
+    //Utilities meliputi PAM dan PLN
+    
+    //konstruktor
     public Utilities(String name, int price){
         super(name,price);
     }
 
+    //Harga rent jika hanya memiliki 1 jenis adalah 1/8 kali harga beli Lot, sedangkan
+    //jika memiliki 2 jenis maka harganya menjadi 1/2 kali harga beli Lot.
     public void rent(){
         
     }
